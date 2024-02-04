d77bc57e6a1642c336caab96ba80ae076337260c
divitkalathil <divitkalathil@gmail.com>
2024-02-04 07:40:42 -0500

    Implemented Web Authentication using MongoDB

--- src/components/Login/Signup.jsx
+++ src/components/Login/Signup.jsx
@@ -2,53 +2,57 @@ import { useState } from 'react';
 import { signupFields } from "./formFields"
 import FormAction from "./FormAction";
 import Input from "./Input";
+import axios from 'axios';
 
-const fields=signupFields;
-let fieldsState={};
+const fields = signupFields;
+let fieldsState = {};
 
-fields.forEach(field => fieldsState[field.id]='');
+fields.forEach(field => fieldsState[field.id] = '');
 
-export default function Signup(){
-  const [signupState,setSignupState]=useState(fieldsState);
+export default function Signup() {
+  const [signupState, setSignupState] = useState(fieldsState);
 
-  const handleChange=(e)=>setSignupState({...signupState,[e.target.id]:e.target.value});
+  const handleChange = (e) => setSignupState({ ...signupState, [e.target.id]: e.target.value });
 
-  const handleSubmit=(e)=>{
+  const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(signupState)
-    createAccount()
+    console.log(signupState);
+    createAccount();
   }
 
-  //handle Signup API Integration here
-  const createAccount=()=>{
+  // Handle Signup API Integration here
+  const createAccount = async () => {
+    try {
+      // Make an API call to your Express server
+      const response = await axios.post('http://localhost:3001/createAccount', signupState);
+      console.log(response.data); // Assuming the server responds with some information
 
+      // Reset the form after successful submission
+      setSignupState(fieldsState);
+    } catch (error) {
+      console.error("Error creating account:", error.message);
+    }
   }
 
-    return(
-        <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
-        <div className="">
-        {
-                fields.map(field=>
-                        <Input
-                            key={field.id}
-                            handleChange={handleChange}
-                            value={signupState[field.id]}
-                            labelText={field.labelText}
-                            labelFor={field.labelFor}
-                            id={field.id}
-                            name={field.name}
-                            type={field.type}
-                            isRequired={field.isRequired}
-                            placeholder={field.placeholder}
-                    />
-
-                )
-            }
-          <FormAction handleSubmit={handleSubmit} text="Signup" />
-        </div>
-
-
-
-      </form>
-    )
+  return (
+    <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+      <div className="">
+        {fields.map(field =>
+          <Input
+            key={field.id}
+            handleChange={handleChange}
+            value={signupState[field.id]}
+            labelText={field.labelText}
+            labelFor={field.labelFor}
+            id={field.id}
+            name={field.name}
+            type={field.type}
+            isRequired={field.isRequired}
+            placeholder={field.placeholder}
+          />
+        )}
+        <FormAction handleSubmit={handleSubmit} text="Signup" />
+      </div>
+    </form>
+  )
 }
