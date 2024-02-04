23289a9b9d22c5db171a793c501a4e22b387be8a
smaraba <smaraba@ncsu.edu>
2024-02-04 04:34:34 -0500

    added recommender frontend

--- src/App.jsx
+++ src/App.jsx
@@ -39,8 +39,7 @@ import BookRecomm from './components/pages/BookRecomm';
 
 function App() {
   return (
-    <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-    <div className="max-w-md w-full space-y-8">
+
      <BrowserRouter>
      <GlobalLayout>
      <Nav />
@@ -69,8 +68,6 @@ function App() {
         <Footer />
         </GlobalLayout>
       </BrowserRouter>
-    </div>
-  </div>
   );
 }
 
